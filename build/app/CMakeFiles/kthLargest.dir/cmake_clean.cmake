f4bd1d8fc35b59809e2798e385fff276a5e7ecf3
adaickalavan <adaickalavan@gmail.com>
2020-02-02 04:15:31 +0800

    Revert "update"
    This reverts commit 8271fca563b0eb1192d4fb4061b788b9681b012a.

---
+++ build/app/CMakeFiles/kthLargest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kthLargest.dir/kthLargest.cpp.o"
+  "../../bin/kthLargest.pdb"
+  "../../bin/kthLargest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kthLargest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/app/CMakeFiles/traverseGrid.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/traverseGrid.dir/traverseGrid.cpp.o"
+  "../../bin/traverseGrid.pdb"
+  "../../bin/traverseGrid"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/traverseGrid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
